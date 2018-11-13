646e22e3cfd5e566526690ce89e304af6dedeb3e
alpino91 <39983382+alpino91@users.noreply.github.com>
2018-11-13 14:26:24 +0100

    2nd

--- prezzo_treno.js
+++ prezzo_treno.js
@@ -7,14 +7,18 @@ var ris = costoUnitario * km;
 var risMin = ris * 0.8
 var risSenior = ris * 0.6;
 
-if (anni < 18 && anni >= 1) {
+if (anni < 18 && anni > 0) {
 document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Sei minorenne: pagherai ' + risMin + ' euro.';
-} else if (Number.isNaN(anni) || Number.isNaN(km)) {
-document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.';
-} else if (anni === 0 || km === 0) {
-document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.';
 } else if (anni >= 65) {
 document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Sei senior: pagherai ' + risSenior + ' euro.' ;
 } else if (anni >= 18 && anni < 65) {
 document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Non puoi usufruire di nessuno sconto. Pagherai ' + ris + ' euro.' ;
+} else if (Number.isNaN(anni)) {
+document.getElementById('result').innerHTML =  'Devi inserire i valori corretti.'
+} else if (anni == 0) {
+document.getElementById('result').innerHTML =  'Devi inserire i valori corretti.'
+}
+
+if (km == 0 || Number.isNaN(km)) {
+  document.getElementById('result').innerHTML =  'Devi inserire i valori corretti.'
 }
