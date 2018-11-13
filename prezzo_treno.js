052c535ba5f3f8a7a30c9dd1504dba6c8aac7484
alpino91 <39983382+alpino91@users.noreply.github.com>
2018-11-13 16:32:19 +0100

    4

--- prezzo_treno.js
+++ prezzo_treno.js
@@ -8,17 +8,17 @@ var risMin = ris * 0.8
 var risSenior = ris * 0.6;
 
 if (anni < 18 && anni > 0) {
-document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Sei minorenne: pagherai ' + risMin + ' euro.';
+  document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Sei minorenne: pagherai ' + risMin + ' euro.';
 } else if (anni >= 65) {
-document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Sei senior: pagherai ' + risSenior + ' euro.' ;
+  document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Sei senior: pagherai ' + risSenior + ' euro.' ;
 } else if (anni >= 18 && anni < 65) {
-document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Non puoi usufruire di nessuno sconto. Pagherai ' + ris + ' euro.' ;
+  document.getElementById('result').innerHTML = 'Hai ' + anni + ' anni. ' + 'Non puoi usufruire di nessuno sconto. Pagherai ' + ris + ' euro.' ;
 } else if (Number.isNaN(anni)) {
-document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.'
+  document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.'
 } else if (anni == 0) {
-document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.'
+  document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.'
 }
 
 if (km == 0 || Number.isNaN(km)) {
-document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.'
+  document.getElementById('result').innerHTML = 'Devi inserire i valori corretti.'
 }
